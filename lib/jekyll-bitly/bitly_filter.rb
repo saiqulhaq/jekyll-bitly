87e173583b80e9803d046de14b39be6e2bd9c589
Saiqul Haq <saiqulhaq@gmail.com>
2023-07-30 10:19:38 +0700

    upgrade

--- lib/jekyll-bitly/bitly_filter.rb
+++ lib/jekyll-bitly/bitly_filter.rb
@@ -2,30 +2,23 @@ require "bitly"
 
 module Jekyll
   class BitlyFilterCache
+    include Singleton
+    extend Dry::Core::Cache
+
     def initialize
-      @result_cache = {}
       config = Jekyll.configuration({})
-      return nil unless config && config["bitly"] && config["bitly"]["username"]
-      @username = config["bitly"]["username"]
-      @key = config["bitly"]["api_key"]
-      Bitly.use_api_version_3
-    end
-
-    # rubocop:disable Style/ClassVars
-    @@instance = BitlyFilterCache.new
-
-    def self.instance
-      @@instance
+      if config && config["bitly"] && config["bitly"]["token"]
+        @bitly_client = Bitly::API::Client.new(token: config["bitly"]["token"])
+      end
     end
 
     def shorten(input)
       input.strip!
-      return input unless @username
-      return @result_cache[input] if @result_cache.key?(input)
-      bitly = Bitly.new(@username, @key)
-      u = bitly.shorten(input, history: 1)
-      @result_cache[input] = u.short_url
-      u.short_url
+      fetch_or_store(self.class.to_s, input) do 
+        bitlink = bitly.shorten(input, history: 1)
+        short_url = bitlink.link
+        short_url
+      end
     end
   end
 
